47b253bfc52302d4ca6bf0c0407d2fefdc0c3cf0
Mert Zararsiz <Mert_Zararsiz@hotmail.com>
2018-05-05 16:35:34 +0200

    Added bulk insertion operation to insert faster Also added a list of errors due corrupted files.

--- main.py
+++ main.py
@@ -1,26 +1,41 @@
 import json
 import glob
 from pymongo import MongoClient
+from pymongo.errors import BulkWriteError
 
-jsonDirectory = 'C:\\Users\\mert\\Downloads\\airlines_complete\\airlines-1490714864176.json\\*.json'
+jsonDirectory = 'C:\\Users\\mert\\Downloads\\airlines_complete\\data\\*.json'
 client = MongoClient('127.0.0.1', 27017)
 db = client.datachallenge
 collection = db.conversation
 
 print('Scanning directory: ', jsonDirectory)
 
-def processAllJsonFiles(directory):
+
+def process_all_json_files(directory):
     jsonFiles = glob.glob(directory)
-    print('Found a total of: ', len(jsonFiles))
+    total = len(jsonFiles)
+    fileCount = 0
+    print('Found a total of: ', total)
+
     for file in jsonFiles:
         with open(file, 'r') as fp:
+            jsonlist = []
+            fileCount += 1
+            print('[{}/{}]Processing: {}'.format(fileCount, total, file))
             for cnt, line in enumerate(fp):
                 try:
                     lineJson = json.loads(line)
-                    result = db.reviews.insert_one(lineJson)
-                    print("Line {}: {}".format(cnt, result.inserted_id))
+                    jsonlist.append(lineJson)
+                    # result = collection.insert_one(lineJson)
+
+                    # print("Line {}: {}".format(cnt, result.inserted_id))
                 except ValueError as e:
-                    print("Line {} ERROR: {}", cnt, e)
+                    print("Line {} ERROR: {}".format(cnt, e))
+            try:
+                result = collection.insert_many(jsonlist)
+                print('Inserted {} out of {} documents.'.format(result.inserted_ids.__len__(), len(jsonlist)))
+            except BulkWriteError as e:
+                print("BulkwriteError: {}".format(e.details))
 
-processAllJsonFiles(jsonDirectory)
 
+process_all_json_files(jsonDirectory)
