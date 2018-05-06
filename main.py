e8471ce760ff5f99c64e2f3c8cf217d91b095068
Mert Zararsiz <Mert_Zararsiz@hotmail.com>
2018-05-06 13:12:22 +0200

    Reverted bulk inserting since I am not sure if it's working properly when duplicate keys occur.

--- main.py
+++ main.py
@@ -3,18 +3,18 @@ import glob
 import pymongo
 
 from pymongo import MongoClient
-from pymongo.errors import BulkWriteError
+from pymongo.errors import BulkWriteError, DuplicateKeyError
 
 jsonDirectory = 'C:\\Users\\mert\\Downloads\\airlines_complete\\data\\*.json'
 client = MongoClient('127.0.0.1', 27017)
 db = client.datachallenge
 collection = db.conversation
 
-
 print('Creating index for field \'id\'. ')
 collection.create_index([("id", pymongo.ASCENDING)], unique=True)
 
 print('Scanning directory: ', jsonDirectory)
+
 
 def process_all_json_files(directory):
     jsonFiles = glob.glob(directory)
@@ -24,23 +24,28 @@ def process_all_json_files(directory):
 
     for file in jsonFiles:
         with open(file, 'r') as fp:
+            duplicatecount = 0
             jsonlist = []
             fileCount += 1
             print('[{}/{}]Processing: {}'.format(fileCount, total, file))
             for cnt, line in enumerate(fp):
                 try:
                     lineJson = json.loads(line)
-                    jsonlist.append(lineJson)
-                    # result = collection.insert_one(lineJson)
+                    # jsonlist.append(lineJson)
+                    result = collection.insert_one(lineJson)
 
                     # print("Line {}: {}".format(cnt, result.inserted_id))
                 except ValueError as e:
                     print("Line {} ERROR: {}".format(cnt, e))
-            try:
-                result = collection.insert_many(jsonlist)
-                print('Inserted {} out of {} documents.'.format(result.inserted_ids.__len__(), len(jsonlist)))
-            except BulkWriteError as e:
-                print("BulkwriteError: {}".format(e.details))
+                except DuplicateKeyError as dke:
+                    duplicatecount += 1
+            if duplicatecount > 0:
+                print('Skipped {} amount of duplicate data.'.format(duplicatecount))
+                # try:
+                #    result = collection.insert_many(jsonlist)
+                #    print('Inserted {} out of {} documents.'.format(result.inserted_ids.__len__(), len(jsonlist)))
+                # except BulkWriteError as e:
+                #    print("BulkwriteError: {}".format(e.details))
 
 
 process_all_json_files(jsonDirectory)
