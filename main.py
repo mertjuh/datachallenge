1c0b6e0025e362be1f891297f92f5521e7df300d
Mert Zararsiz <mert_zararsiz@hotmail.com>
2018-05-28 18:11:03 +0200

    Working average sentiment analysis(2)

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import databaseimporter as importer
 
 from config import jsonDirectory
 from conversation import find_average_conversation_length, import_conversation_trees_from_db
-from sentiment_test import get_sentiment_for
+from sentiment_test import get_average_sentiment_for
 
 
 def create_database():
@@ -20,15 +20,12 @@ def create_conversation_database():
 # create_database()  # run this only once!
 # create_conversation_database()  # run this once too.
 
-user_id = 22536055
+user_id = 106062176 # this is airfrance, americanair: 22536055
 
 print("Importing trees..")
 user_trees = import_conversation_trees_from_db(user_id)
-
 print("Finding conversation length..")
 conv_length = find_average_conversation_length(user_trees)
-
 print("Average conversation length is: {}".format(conv_length))
-
 print("Finding Sentiment score..")
-get_sentiment_for(user_trees)  # this function gets the sentiment basically
+get_average_sentiment_for(user_trees)  # this function gets the sentiment basically
