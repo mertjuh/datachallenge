9bf4ea2dee50dc1a85bf757a589cf352e2e645f2
Mert Zararsiz <mert_zararsiz@hotmail.com>
2018-06-12 14:54:12 +0200

    Nice way to display progress.

--- analyzer.py
+++ analyzer.py
@@ -7,19 +7,18 @@ def find_sentiment_for_ids(id_list, topics=None):
     print("Doing analysis for: {} using filter topic: {}".format(id_list, topics))
     list_length = len(id_list)
     return_info = []
-    for index, id in enumerate(id_list,1):
-        trees = import_conversation_trees_from_db(id,
-                                                  filter=topics)
+    for index, id in enumerate(id_list, 1):
+        screen_name = collection.find_one({"user.id": id})['user']['screen_name']
+        print("[{}/{}] Starting analysis for: {}".format(index, list_length, screen_name))
+
+        trees = import_conversation_trees_from_db(id, filter=topics)
         sent_score = get_average_sentiment_for(trees)
         sent_score_with_filtered_id = get_average_sentiment_for(trees, ignore_id=id)
-        result = {
+        return_info.append({
             'id': id,
-            'screen_name': collection.find_one({"user.id": id})['user']['screen_name'],
+            'screen_name': screen_name,
             'sentiment_score_old': sent_score,
             'topics': topics,
             'sentiment_score_new': sent_score_with_filtered_id
-        }
-        print("[{}/{}] Finished analysis for: {}".format(index, list_length, result['screen_name']))
-
-        return_info.append(result)
+        })
     return return_info
